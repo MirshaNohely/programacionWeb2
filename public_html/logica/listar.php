ac348cc1a932058f67dde710a7af19aad16d351f
Mirsha Cortés <72098013+MirshaNohely@users.noreply.github.com>
2023-05-24 23:52:16 -0600

    actualizando

---
+++ public_html/logica/listar.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listar Registros</title>
+</head>
+<body>
+    <h1>Listar Registros</h1>
+
+    <!-- Botón para listar registros -->
+    <form action="listar_registros.php" method="post">
+        <input type="submit" value="Listar Registros">
+    </form>
+</body>
+</html>
